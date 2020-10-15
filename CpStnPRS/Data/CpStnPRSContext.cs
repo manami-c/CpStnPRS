0a5298d5be0e1d491d5f33c16f85af6d4eb25477
Manami Crowley <manami.faye@gmail.com>
2020-10-15 15:12:09 -0400

    Add project files.

---
+++ CpStnPRS/Data/CpStnPRSContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CpStnPRS.Models;
+
+namespace CpStnPRS.Data
+{
+    public class CpStnPRSContext : DbContext
+    {
+        public CpStnPRSContext (DbContextOptions<CpStnPRSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<CpStnPRS.Models.User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(e =>
+            {
+                e.HasIndex(p => p.Username).IsUnique();
+            });
+        }
+    }
+}
