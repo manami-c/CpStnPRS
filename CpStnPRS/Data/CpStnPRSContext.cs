df60fe43b258030cc0ee68ac0b208f0541b2ba78
Manami Crowley <manami.faye@gmail.com>
2020-10-15 16:08:06 -0400

    added Vendors

--- CpStnPRS/Data/CpStnPRSContext.cs
+++ CpStnPRS/Data/CpStnPRSContext.cs
@@ -15,12 +15,20 @@ namespace CpStnPRS.Data
         }
 
         public virtual DbSet<CpStnPRS.Models.User> Users { get; set; }
+
+        public virtual DbSet<CpStnPRS.Models.Vendor> Vendors { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             builder.Entity<User>(e =>
             {
                 e.HasIndex(p => p.Username).IsUnique();
             });
+            builder.Entity<Vendor>(e =>
+            {
+                e.HasIndex(p => p.Code).IsUnique();
+            });
         }
+       
     }
 }
--- CpStnPRS/Migrations/20201015195654_added Vendors.Designer.cs
+++ CpStnPRS/Migrations/20201015195654_added Vendors.Designer.cs
@@ -3,14 +3,16 @@ using CpStnPRS.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CpStnPRS.Migrations
 {
     [DbContext(typeof(CpStnPRSContext))]
-    partial class CpStnPRSContextModelSnapshot : ModelSnapshot
+    [Migration("20201015195654_added Vendors")]
+    partial class addedVendors
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
